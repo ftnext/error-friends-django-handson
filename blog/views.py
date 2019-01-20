aba5e79b275ac6234ab20e29dfa6e60dae3512bd
ftnext <takuyafjp+develop@gmail.com>
2019-01-20 22:01:03 +0900

    [feat] 記事一覧用のビュー関数を追加（TemplateDoesNotExistが発生）

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
+ 
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') # 1にあたる
+    return render(request, 'blog/post_list.html', {'posts': posts}) # 2と3にあたる
+    

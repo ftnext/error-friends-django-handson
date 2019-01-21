1407627057cd748d4998140575ccff812f7addbb
ftnext <takuyafjp+develop@gmail.com>
2019-01-21 20:41:13 +0900

    [feat] 記事が保存されるようにpost_new関数を更新

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
 from .forms import PostForm
@@ -8,5 +9,14 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts}) # 2と3にあたる
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})

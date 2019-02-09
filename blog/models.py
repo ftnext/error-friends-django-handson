4082d4bd96eeed25ffccb96f860be1c98bce3505
ftnext <takuyafjp+develop@gmail.com>
2019-02-09 10:12:11 +0900

    [feat] モデルに画像のフィールドimageを追加

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,8 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image = models.ImageField(
+            upload_to='photos/%Y/%m/%d', blank=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
